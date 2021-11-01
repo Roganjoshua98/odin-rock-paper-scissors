02d58f19ee270f0ee4ec987f5f0b6962979b846f
Joshua <joshuareyestraverso@hotmail.com>
2021-11-01 15:55:03 +0000

    implement the odin spec

---
+++ scripts/rps.js
@@ -0,0 +1,54 @@
+const MOVES = ["ROCK", "PAPER", "SCISSORS"];
+const WIN = "WIN";
+const LOSE = "LOSE";
+
+function computerPlay() {
+    let move = Math.floor(Math.random() * MOVES.length);
+    return MOVES[move];
+}
+
+function playerPlay() {
+    let move = prompt("Play your move from 'rock', 'paper', or 'scissors'.").toUpperCase();
+    while (!MOVES.includes(move)) {
+        alert("Please enter a valid move.");
+        move = prompt("Play your move from 'rock', 'paper', or 'scissors'.").toUpperCase();
+    }
+    return move;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) { alert(`It's a draw! You both played ${playerSelection}. Try again.`); }
+
+    else if (playerSelection === "ROCK" && computerSelection === "PAPER"
+    || playerSelection === "PAPER" && computerSelection === "SCISSORS"
+    || playerSelection === "SCISSORS" && computerSelection === "ROCK") { 
+        alert(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        return LOSE;
+    }
+    
+    else { 
+        alert(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return WIN; 
+    }
+}
+
+function game() {
+    let rounds = +prompt("How many rounds do you want to play?");
+    while (rounds < 1 || isNaN(rounds)) { 
+        alert("Not a valid input! Please enter a positive number.");
+        rounds = +prompt("How many rounds do you want to play?");
+    }
+
+    let counter = 0; let playerWins = 0; let computerWins = 0;
+    let result;
+    while (counter < rounds) {
+        result = playRound(playerPlay(), computerPlay());
+        if (result === WIN) { playerWins++; counter++; }
+        else if (result === LOSE) {  computerWins++; counter++; }
+    }
+    if (playerWins > computerWins) { alert(`Player won with ${playerWins} points. Congrats!`); }
+    else if (playerWins < computerWins) { alert(`Computer won with ${computerWins} points. Better luck next time!`) }
+    else { alert("Game ended in a draw.") }
+}
+
+game();
