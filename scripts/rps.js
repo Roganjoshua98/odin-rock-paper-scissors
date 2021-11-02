1fd93c0bfd31bb51f1683d454befce6f07146217
Joshua <joshuareyestraverso@hotmail.com>
2021-11-02 11:27:41 +0000

    janitor

--- scripts/rps.js
+++ scripts/rps.js
@@ -3,11 +3,13 @@ const WIN = "WIN";
 const LOSE = "LOSE";
 
 function computerPlay() {
+    /** Pick a random int 0 to 2 and pick it from the MOVES list */
     let move = Math.floor(Math.random() * MOVES.length);
     return MOVES[move];
 }
 
 function playerPlay() {
+    /** Player enters valid move (with check) */
     let move = prompt("Play your move from 'rock', 'paper', or 'scissors'.").toUpperCase();
     while (!MOVES.includes(move)) {
         alert("Please enter a valid move.");
@@ -17,15 +19,16 @@ function playerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Draw
     if (playerSelection === computerSelection) { alert(`It's a draw! You both played ${playerSelection}. Try again.`); }
-
+    // Lose
     else if (playerSelection === "ROCK" && computerSelection === "PAPER"
     || playerSelection === "PAPER" && computerSelection === "SCISSORS"
     || playerSelection === "SCISSORS" && computerSelection === "ROCK") { 
         alert(`You lose! ${computerSelection} beats ${playerSelection}.`);
         return LOSE;
     }
-    
+    // Win
     else { 
         alert(`You win! ${playerSelection} beats ${computerSelection}.`);
         return WIN; 
@@ -33,12 +36,14 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    /** Main game loop */
+    // Declare number of rounds
     let rounds = +prompt("How many rounds do you want to play?");
     while (rounds < 1 || isNaN(rounds)) { 
         alert("Not a valid input! Please enter a positive number.");
         rounds = +prompt("How many rounds do you want to play?");
     }
-
+    // Game loop
     let counter = 0; let playerWins = 0; let computerWins = 0;
     let result;
     while (counter < rounds) {
@@ -46,6 +51,7 @@ function game() {
         if (result === WIN) { playerWins++; counter++; }
         else if (result === LOSE) {  computerWins++; counter++; }
     }
+    // Declare winner
     if (playerWins > computerWins) { alert(`Player won with ${playerWins} points. Congrats!`); }
     else if (playerWins < computerWins) { alert(`Computer won with ${computerWins} points. Better luck next time!`) }
     else { alert("Game ended in a draw.") }
